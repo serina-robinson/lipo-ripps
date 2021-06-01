f0903046bb51e2bb9161e498becba90521beadaf
Serina Robinson <robinsonserinalee@gmail.com>
2021-06-01 20:50:21 +0200

    New figure layout

--- scripts/20210530_Figure1.r
+++ scripts/20210530_Figure1.r
@@ -72,20 +72,22 @@ bpldf <- data.frame(sort(colSums(mat_bool[,2:ncol(mat_bool)]), decreasing = T)[1
   rownames_to_column()
 colnames(bpldf) <- c("PFAM", "Count")
 
-bpldf <- bpldf %>%
+bpldf <- bpldf %>% 
   dplyr::filter(PFAM != "NA") %>%
-  dplyr::mutate(coloring = ifelse(PFAM %in% "PF00583_Acetyltransf_1", "#E41A1C", "gray60")) %>%
-  dplyr::mutate("% BGCs containing PFAM" = round((Count/2346) * 100, 2))
+  dplyr::filter(PFAM != "NA_NA") %>%
+  dplyr::mutate(coloring = ifelse(PFAM %in% "PF00583_Acetyltransf_1", "forestgreen", "gray60")) %>%
+  dplyr::mutate("% BGCs containing PFAM" = round((Count/2347) * 100, 2))
 
-bpldf$PFAM <- factor(bpldf$PFAM, levels = as.character(bpldf$PFAM))
+bpldf$PFAM <- factor(bpldf$PFAM, levels = rev(as.character(bpldf$PFAM)))
 
-pdf("output/Supplementary_fig_S1.pdf", width = 20, height = 8)
+pdf("output/Supplementary_fig_S1.pdf", width = 8.5, height = 12)
 bpl_s1 <- ggplot(data = bpldf, aes(x = PFAM, y = `% BGCs containing PFAM`)) + 
   geom_bar(stat = "identity", fill = bpldf$coloring) + 
   theme_pubr() +
-  theme(axis.text.x = element_text(angle = 40, hjust = 1)) +
+ # theme(axis.text.x = element_text(angle = 70, hjust = 1)) +
   #scale_x_discrete(expand = expansion(mult = c(0, 0))) + 
-  scale_y_continuous(expand = expansion(mult = c(0, 0)))
+  scale_y_continuous(expand = expansion(mult = c(0, 0))) +
+  coord_flip()
 bpl_s1
 dev.off()
 
@@ -108,7 +110,7 @@ bpl_maturases$PFAM <- gsub("_", " ", bpl_maturases$PFAM)
 bpl_maturases$PFAM[grep(" ", bpl_maturases$PFAM)]
 bpl_maturases$`Protein family` <- c("Radical SAM (PF04055)",
                "DUF4135 (PF13575)",
-               "Acetyltransferase (PF00583)",
+               "Acyltransferase (PF00583)",
                "YcaO (PF02624)",
                "Nitroreductase (PF00881)",
                "Enoyl-ACP reductase (PF13561)",
@@ -117,15 +119,15 @@ bpl_maturases$`Protein family` <- c("Radical SAM (PF04055)",
                "Amine oxidoreductase (PF01593)")
    
 bpl_maturases$Family<- factor(bpl_maturases$`Protein family`, 
-                              levels = c("Radical SAM (PF04055)",
+                              levels = rev(c("Radical SAM (PF04055)",
                                         "DUF4135 (PF13575)",
-                                        "Acetyltransferase (PF00583)",
+                                        "Acyltransferase (PF00583)",
                                         "YcaO (PF02624)",
                                         "Nitroreductase (PF00881)",
                                         "Enoyl-ACP reductase (PF13561)",
                                         "Methyltransferase (PF13649)",
                                         "Short-chain dehydrogenase (PF00106)",
-                                        "Amine oxidoreductase (PF01593)"))
+                                        "Amine oxidoreductase (PF01593)")))
 
 bpl <- ggplot(data = bpl_maturases, aes(x = Family, y = `% BGCs containing PFAM`)) + 
   geom_bar(stat = "identity", fill = bpl_maturases$coloring) + 
@@ -205,8 +207,11 @@ plotdf <- data.frame(grouping_id = rownames(pc.bray$points),
 
 # Set custom color palette for plotting
 pal <- colorRampPalette(brewer.pal(8, "Set1"))(8)
+pal <- colorRampPalette(brewer.pal(8, "Set1"))(8)
 pal
-pal2 <- c(pal[c(1:5)], "black", "gray80")
+
+pal2 <- c(pal[c(1:2, 4:5, 8)], "seagreen2", "gray80",  "black")
+pal2
 
 # PCoA of taxonomy assignments (phylum level)
 prod <- ggplot() +
@@ -228,7 +233,7 @@ prod <- ggplot() +
         y = PC2,
         label = known_product)) +
   guides(colour = guide_legend(override.aes = list(size=5),
-                               ncol = 1))
+                               ncol = 2))
 prod
 
 
@@ -252,11 +257,11 @@ acetyl <- ggplot() +
                 y = PC2,
                 color = PF00583_Acetyltransf_1)) +
   theme_pubr() +
-  scale_color_viridis(name = "GNAT-acyltransferases \n (PF00583) \n per BGC",
+  scale_color_viridis(name = "GNAT-acyltransferases\n(PF00583)\nper BGC",
                       direction = -1, 
                       option = "magma",
                       end = 0.9,
-                      na.value = "gray80") +
+                      na.value = "gray80") 
   #theme(axis.title.x = element_blank())
 acetyl
 
@@ -269,7 +274,7 @@ c39 <- ggplot() +
                y = PC2,
                color = PF03412_Peptidase_C39)) +
   theme_pubr() +
-  scale_color_viridis(name = "Peptidase C39 \n (PF03412) \n per BGC",
+  scale_color_viridis(name = "Peptidase C39\n(PF03412)\nper BGC",
                       direction = -1, 
                       option = "magma",
                       end = 0.9,
@@ -284,7 +289,7 @@ abc <- ggplot() +
                y = PC2,
                color = PF00005_ABC_tran)) +
   theme_pubr() +
-  scale_color_viridis(name = "ABC trans. \n  (PF00005) \n per BGC",
+  scale_color_viridis(name = "ABC trans.\n(PF00005)\n per BGC",
                       direction = -1, 
                       option = "magma",
                       end = 0.9,
@@ -300,7 +305,7 @@ sam <- ggplot() +
                y = PC2,
                color = PF04055_Radical_SAM)) +
   theme_pubr() +
-  scale_color_viridis(name ="Radical SAMs \n (PF04055) \n per BGC",
+  scale_color_viridis(name ="Radical SAMs\n (PF04055)\n per BGC",
                       direction = -1,
                       option = "magma",
                       end = 0.9,
@@ -322,7 +327,7 @@ nif11 <- ggplot() +
   theme_pubr() +
   #theme(axis.title.x = element_blank()) +
   scale_color_manual(values = magma(8)[c(5, 7)],
-                     name = "RiPP precursor type")
+                     name = "RiPP\nprecursor\ntype")
 nif11
 
 
@@ -345,16 +350,18 @@ duf4135
 
 # Set additional colors in custom palette
 pal_distinct <- palette(colorRampPalette(brewer.pal(8,"Set2"))(8))
-pal3 <- c(pal_distinct[2:8], "navyblue", "orange", "maroon", "black", "goldenrod", distinctColorPalette(8))
-colorblind <- c(pal[1], "#CC6677",  "#999933",  "#332288", "#AA4499",  
-                "#117733", pal_distinct[6:7],
+colorblind <- c(pal[1], "#CC6677",  "#999933",  "steelblue4", "#AA4499",  
+                 pal_distinct[5:7], 
                 "#882255", "#6699CC",  pal_distinct[2:3], "gray40",
-                "navyblue", "#661100",  "#88CCEE", pal_distinct[4:5],  
-                "orange", "goldenrod", "purple4", "#888888", "#DDCC77")
+                "navyblue", "mistyrose1", "#88CCEE", pal_distinct[4], #"#661100", 
+                "orange", "goldenrod", "purple4", "lightseagreen")
+# "#117733"
+show_col(colorblind)
 plotdf$phylum[is.na(plotdf$phylum)] <- "Other"
 
 cr_df <- data.frame(sort(table(plotdf$phylum), decreasing = T), stringsAsFactors = F) 
 colnames(cr_df) <- c("Phylum", "Freq")
+sum(cr_ord$Freq)
 cr_df$Phylum <- as.character(cr_df$Phylum)
 cr_ord <- cr_df[order(cr_df$Phylum),] # order alphabetically
 cr_ord$lab <- round((cr_ord$Freq/sum(cr_ord$Freq) * 100), 1)
@@ -373,6 +380,7 @@ donut
 phygraph <- ggplot(data = plotdf) +
   geom_point(data = plotdf,
              alpha = 0.8,
+             size = 2,
              aes(
                x = PC1,
                y = PC2,
@@ -380,12 +388,7 @@ phygraph <- ggplot(data = plotdf) +
   scale_color_manual(values = colorblind,
                      name = "Phylum") +
   theme_pubr()  +
-  theme(legend.position = "none",
-        axis.title = element_text(size = 14),
-        legend.text=element_text(size=14),
-        legend.title = element_text(size = 14),
-  ) +
-  guides(colour = guide_legend(override.aes = list(size=4),
+  guides(colour = guide_legend(override.aes = list(size= 3),
                                ncol = 1))
   #guides(colour = guide_legend(override.aes = list(size=2),))
 phygraph
@@ -393,59 +396,70 @@ phygraph
 
 phygraph <- phygraph +
   ggtitle('A') +
-  theme(axis.text = element_text(size = 12),
-        #axis.title = element_blank(),
-        #plot.title = element_blank(),
-        legend.text = element_text(size = 14),
-        legend.position = "right")
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18),
+        legend.text=element_text(size = 14),
+        legend.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.position = "none")
 donut <- donut +
   ggtitle('Z') +
-  theme(axis.text = element_text(size = 12),
+  theme(axis.text = element_text(size = 16),
         axis.title = element_blank(),
         plot.title = element_blank(),
         legend.position = "none")
 prod <- prod +
   ggtitle('B') +
-  theme(axis.text = element_text(size = 12),
-        axis.title = element_text(size = 14),
-        plot.title = element_text(size = 20))
-sam <- sam + 
-  ggtitle('C') +
-  theme(axis.text = element_text(size = 12),
-        axis.title = element_text(size = 14),
-        plot.title = element_text(size = 20),
-        legend.position = "right")
-c39 <- c39 + 
-  ggtitle('C') +
-  theme(axis.text = element_text(size = 12),
-        axis.title = element_text(size = 14),
-        plot.title = element_text(size = 20),
-        legend.position = "right")
-abc <- abc +
-  ggtitle('D') +
-  theme(axis.text = element_text(size = 12),
-        axis.title = element_text(size = 14),
-        plot.title = element_text(size = 20),
-        legend.position = "right")
-nif11 <- nif11 +
-  ggtitle('E') +
-  theme(axis.text = element_text(size = 12),
-        axis.title = element_text(size = 14),
-        plot.title = element_text(size =  20),
-        legend.position = "right")
-acetyl <- acetyl +
-  ggtitle('F') +
   theme(axis.text = element_text(size = 16),
         axis.title = element_text(size = 18),
-        plot.title = element_text(size = 20),
-        legend.position = "right")
+        plot.title = element_text(size = 24))
 bpl <- bpl +
-  ggtitle('G') +
+  ggtitle('C') +
   theme(axis.text = element_text(size = 16),
-        axis.title = element_text(size = 20),
+        axis.title = element_text(size = 18),
+        plot.title = element_text(size = 24),
         axis.title.y = element_blank()) +
   coord_flip()
 bpl
+# sam <- sam + 
+#   ggtitle('C') +
+#   theme(axis.text = element_text(size = 12),
+#         axis.title = element_text(size = 14),
+#         plot.title = element_text(size = 20),
+#         legend.position = "right")
+c39 <- c39 + 
+  ggtitle('F') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        legend.position = "right")
+abc <- abc +
+  ggtitle('E') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        legend.position = "right")
+nif11 <- nif11 +
+  ggtitle('D') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size =  24),
+        legend.text=element_text(size=16),
+        legend.title = element_text(size = 14),
+        legend.position = "right")
+acetyl <- acetyl +
+  ggtitle('G') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        legend.position = "right")
+
 #ZZZZZZ
 # layout <- '
 # AAAAAAAABBB
@@ -455,27 +469,120 @@ bpl
 #EEGGHH
 
 layout <- '
-AAAAAABBCCCCCC
-DDDDEEEHHHHHHH
-DDDDEEEHHHHHHH
-FFFFGGGHHHHHHH
-FFFFGGGHHHHHHH'
+AAAAAABBBCCCCCCCC
+DDDDDEEEHHHHHHHHH
+DDDDDEEEHHHHHHHHH
+FFFFFGGGHHHHHHHHH
+FFFFFGGGHHHHHHHHH'
 
-pdf("output/Figure1.pdf", width = 20, height = 14)
+# pdf("output/Figure1.pdf", width = 21, height = 14)
+# wrap_plots(
+#   A = prod,
+#   B = phygraph,
+#   C = donut,
+#   D = nif11,
+#   #D = abc,
+#   E = abc,
+#   F = c39,
+#   G = acetyl,
+#   H = bpl, 
+#   heights = c(8,1.5,1.5,1.5,1.5),
+#  # widths = c(3,1,1,
+#   #           1,1,3),
+#   # widths = c(1, 1, 2, 1),
+#   # 
+#   design = layout)
+# dev.off()
+
+
+layout1 <- '
+AAAABBBB
+DDDDCCC#'
+
+# New figure (as per Flo's specifications)
+pdf("output/Figure1_part1.pdf", width = 15, height = 12)
 wrap_plots(
-  A = prod,
-  B = phygraph,
-  C = donut,
-  D = c39,
-  #D = abc,
+  A = phygraph,
+  B = donut,
+  C = bpl,
+  D = prod,
+  heights = c(3,3),
+  widths = c(2,1),
+  design = layout1)
+dev.off()
+
+# pdf("output/Figure1_part1.pdf", width = 14, height = 10)
+# phygraph + donut + prod + bpl + plot_layout(widths = 1)
+# dev.off()
+
+layout2 <-'
+EEEFFF
+GGGHHH'
+
+pdf("output/Figure1_part2.pdf", width = 15, height = 10)
+wrap_plots(  
   E = nif11,
   F = abc,
-  G = acetyl,
-  H = bpl, 
-  heights = c(8,1.5,1.5,1.5,1.5),
- # widths = c(3,1,1),
+  G = c39,
+  H = acetyl,
+#  heights = c(3,4,2,2),
+  widths = c(1,1,1,1),
+ # widths = c(3,1,1,
+  #           1,1,3),
   # widths = c(1, 1, 2, 1),
-  # 
-  design = layout)
+  #
+  design = layout2)
 dev.off()
+
+
+c39 <- c39 + 
+  ggtitle('F') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=12),
+        legend.title = element_text(size = 14),
+        legend.position = "top")
+abc <- abc +
+  ggtitle('E') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=12),
+        legend.title = element_text(size = 14),
+        legend.position = "top")
+nif11 <- nif11 +
+  ggtitle('D') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size =  24),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        legend.position = "top")
+acetyl <- acetyl +
+  ggtitle('G') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=12),
+        legend.title = element_text(size = 14),
+        legend.position = "top")
+
+layout3 <-'
+EEEFFFGGGHHH'
+pdf("output/Figure1_part2_horiz.pdf", width = 15, height = 5)
+wrap_plots(  
+  E = nif11,
+  F = abc,
+  G = c39,
+  H = acetyl,
+  #  heights = c(3,4,2,2),
+  widths = c(1,1,1,1),
+  # widths = c(3,1,1,
+  #           1,1,3),
+  # widths = c(1, 1, 2, 1),
+  #
+  design = layout3)
+dev.off()
+
 
