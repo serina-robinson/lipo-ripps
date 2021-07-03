7937721bafa4048c8d03949fce218250825b0d0d
Serina Robinson <robinsonserinalee@gmail.com>
2021-07-03 12:33:39 +0200

    data processing scripts added

---
+++ scripts/20210628_Figure1_data_prep.R
@@ -0,0 +1,160 @@
+# Install packages
+pacman::p_load("tidyverse", "Biostrings", "data.table")
+
+# RODEO v 2.2.0.
+# python3 rodeo_main.py -fn 8 -j 4 -out 5000_NHLP_accessions_pull.txt
+# python3 rodeo_main.py -fn 8 -j 4 -out 5849_Nif11_accessions_pull.txt
+
+# Read in the rodeo2 output
+nhlp_dat <- read_csv("data/NHLP_all_queries/5000_NHLP_nbsize_8_rodeo2/main_co_occur.csv") %>%
+  janitor::clean_names()
+
+nif11_dat <- read_csv("data/Nif11/5849_Nif11_nbsize_8_rodeo2_out/main_co_occur.csv") %>%
+  janitor::clean_names()
+
+# Read in precursors with L....[GSA][GSA] cleavage site
+nhlp <- readAAStringSet("data/combined_Nif11_NHLP_cleavage_motifs/1574_NHLP_no_cat_with_cleavage_site.faa")
+nif11 <- readAAStringSet("data/combined_Nif11_NHLP_cleavage_motifs/2819_Nif11_with_cleavage_site.faa")
+nhlp_queries <- unique(word(names(nhlp), sep = " ", 1))
+nif11_queries <- unique(word(names(nif11), sep = "\\:", 1))
+
+# NHLP and Nif11 trimmed
+nhlp_trim <- nhlp_dat %>%
+  dplyr::filter(query %in% nhlp_queries)
+length(unique(nhlp_trim$query)) # 1565
+
+nif11_trim <- nif11_dat %>%
+  dplyr::filter(query %in% nif11_queries)
+length(unique(nif11_trim$query)) # 2163 
+
+# Search for NHase_beta hits
+nhase_beta <- nhlp_trim %>%
+  dplyr::filter(grepl("NHase_beta", name1) | 
+                  grepl("NHase_beta", name2) | 
+                  grepl("NHase_beta", name3)) %>%
+  dplyr::select(query) %>%
+  pull() %>%
+  unique()
+
+# Combine everything
+# Read in polytheonamide
+poly <- read_csv("data/characterized_nps/Entotheonella_polytheonamide_nb.csv")
+aero <- read_csv("data/characterized_nps/Microvirgula_aeronamide_nb.csv") %>%
+  janitor::clean_names()
+ 
+dat_comb <- poly %>% # add polytheonamide cluster
+  bind_rows(aero) %>%  # add aeronamide cluster
+  bind_rows(nhlp_trim) %>%
+  dplyr::filter(!query %in% nhase_beta) %>% # remove NHase beta
+  bind_rows(nif11_trim) 
+
+# Pull genus list to merge with Genome Taxonomy Database
+dat_tax <- dat_comb %>%
+  mutate(genus_1 = case_when(grepl("Candidatus|^cf\\.|^aff\\.|uncultured|^bacterium", genus_species) ~ word(genus_species, sep = " ", 2),
+                             grepl("\\]", genus_species) ~ gsub("\\[", "", word(genus_species, sep = "\\]", 1)),
+                             TRUE ~ word(genus_species, sep = " ", 1))) %>%
+  select(genus_1) %>%
+  pull() %>%
+  unique()
+
+# Remove taxonomy exceptions
+exceptions <- dat_tax[!grepl("[A-Z]", dat_tax)] # remove if it isn't a Latin genus name
+dat_tax_fin <- dat_tax[!grepl(paste0(paste0("^",exceptions), collapse = "|"), dat_tax)]
+
+# GTDB taxonomy 
+unparsed_tax <- fread("data/taxonomy/gtdb_bac120_tax.tsv", data.table = F,
+                      fill = TRUE, header = F, sep = "") %>%
+  dplyr::mutate(unparse_with_GCF = word(V1, sep = ";s__", 1)) %>%
+  dplyr::mutate(unparse = word(unparse_with_GCF, sep = "d__Bacteria;", 2)) %>%
+  dplyr::select(unparse) %>%
+  distinct() %>%
+  pull() 
+
+gtdb_tax <- lapply(1:length(dat_tax_fin), function(x) {
+  unparsed_tax[grep(dat_tax_fin[x], unparsed_tax)[1]]})
+
+newdf <- data.frame(dat_tax_fin, unlist(gtdb_tax))
+colnames(newdf) <- c("genus_key", "gtdb_tax")
+
+# Merge taxonomy with neighborhoods
+dat_match <- dat_comb %>%
+  mutate(genus_1 = case_when(grepl("Candidatus|^cf\\.|^aff\\.|uncultured|^bacterium", genus_species) ~ word(genus_species, sep = " ", 2),
+                             grepl("\\]", genus_species) ~ gsub("\\[", "", word(genus_species, sep = "\\]", 1)),
+                             TRUE ~ word(genus_species, sep = " ", 1))) %>%
+  dplyr::left_join(., newdf, by = c("genus_1" = "genus_key")) %>%
+  dplyr::mutate(phylum = word(word(word(gtdb_tax, sep = "p__", 2), sep = ";", 1), sep = "_", 1)) %>%
+  dplyr::mutate(class = word(word(word(gtdb_tax, sep = "c__", 2), sep = ";", 1), sep = "_", 1)) %>%
+  dplyr::mutate(order = word(word(word(gtdb_tax, sep = "o__", 2), sep = ";", 1), sep = "_", 1)) %>%
+  dplyr::mutate(family = word(word(word(gtdb_tax, sep = "f__", 2), sep = ";", 1), sep = "_", 1))
+
+# Check how many NAs
+dat_na <- dat_match %>%
+  filter(is.na(gtdb_tax))
+
+# Now get counts for PFAMs 
+dat_trim <- dat_match %>%
+  dplyr::filter(!is.na(pfam_id1)) %>%
+  arrange(query) %>%
+  dplyr::mutate(idvar = paste0(nucleotide_acc, "_", protein_acc)) %>%
+  group_by(idvar) %>%
+  dplyr::slice(1)
+
+dat_pfam_tally <- dat_trim %>%
+  select(-query) %>%
+  distinct(.keep_all = T) %>%
+  mutate(pfam_long = paste0(name1, "_", pfam_id1)) %>% # need to find a way to do this for name2
+  group_by(nucleotide_acc) %>%
+  dplyr::add_count(pfam_long) %>%
+  mutate(genvar = paste0(genus_species, '_', nucleotide_acc))
+
+top_pfams <- sort(table(dat_pfam_tally$pfam_long), decreasing = T)
+pfams_to_keep <- names(top_pfams)[top_pfams > 9]
+length(pfams_to_keep) 
+
+# WHICH KEEP - DO NOT INCLUDE IF QUERY APPEARS MORE THAN ONCE IN PROTEIN ACC
+quers <- unique(dat_match$query) #3724 queries 
+
+# Count how many BGCs
+dat_quers <- dat_match %>%
+  dplyr::filter(protein_acc %in% quers) %>%
+  mutate(unique_idvar = paste0(nucleotide_acc, "_", protein_acc)) %>%
+  group_by(unique_idvar) %>%
+  dplyr::slice(1) # only one combination of nucleotide acc and query 
+# in the same neighborhood, to prevent double-counting
+
+dat_pfam_tally_trim <- dat_match %>%
+  dplyr::filter(query %in% dat_quers$query) %>%
+  dplyr::filter(!pfam_id1 %in% c('PF02979', 'PF14407', 'PF07862')) %>%
+  dplyr::filter(!pfam_id2 %in% c('PF02979', 'PF14407', 'PF07862')) %>%
+  #  "PF02211", # NHase_beta should already be removed 
+  #  "PF02979", "PF14407", # NHLP precursors
+  #  "PF07862", # Nif11 precursor
+  dplyr::filter(!is.na(pfam_id1)) %>%
+  dplyr::mutate(unid = paste0(nucleotide_acc, "_", protein_acc, "_", start)) %>%
+  distinct(unid, .keep_all = T) %>%
+  mutate(pfam_long = paste0(pfam_id1, "_", name1))
+length(unique(dat_pfam_tally_trim$query)) # 2345, excluding neighborhoods of 1
+
+precs <- dat_match %>%
+  dplyr::filter(query %in% dat_pfam_tally_trim$query) %>%
+  dplyr::filter(protein_acc %in% dat_pfam_tally_trim$query) %>%
+  distinct(, .keep_all = T) %>%
+  mutate(pfam_long = paste0(pfam_id1, "_", name1))
+
+length(unique(precs$query)) # 2347 unique queries
+
+# Condense into clans
+clans <- read_tsv("data/pfam34_clans.tsv")
+
+dat_pfam_clan <- dat_pfam_tally_trim %>%
+  dplyr::bind_rows(precs) %>%
+  distinct(.keep_all = T) %>%
+  left_join(., clans, by = c("pfam_id1" = "pfamA_acc")) %>%
+  dplyr::mutate(clan_keep = case_when(clan_acc == "NULL" ~ pfam_long,
+                                      TRUE ~ paste0(clan_acc, "_", clan_id))) %>%
+  dplyr::mutate(genvar = paste0(query, "_", nucleotide_acc, "_", genus_species))# %>%
+
+length(unique(dat_pfam_clan$nucleotide_acc)) # 1922 genomes
+length(unique(dat_pfam_clan$genvar)) # 2347 BGCs
+
+write_csv(dat_pfam_clan, "data/combined_Nif11_NHLP_cleavage_motifs/1922_full_Nif11_NHLP_genome_nbs_with_clans_precursors.csv")
---
+++ scripts/20210628_Figure1_make_plot.r
@@ -0,0 +1,499 @@
+# Install packages
+pacman::p_load("tidyverse", "Biostrings", "randomcoloR","umap",
+               "data.table", "vegan", "RColorBrewer", "ggrepel",
+               "scales", "ggpubr", "patchwork", "caret", "viridis")
+
+# Set random seed
+set.seed(1234)
+
+# Read in and clean-up the dataset
+dat <- read_csv("data/combined_Nif11_NHLP_cleavage_motifs/1922_full_Nif11_NHLP_genome_nbs_with_clans_precursors.csv") %>%  
+  janitor::clean_names() %>%
+  dplyr::mutate(grouping_id = paste0(query, "_", genus_species)) %>%
+  group_by(grouping_id) %>%
+  dplyr::add_count(name = "pfam_count", pfam_long) %>%
+  dplyr::add_count(name = "clan_count", clan_keep) %>%
+  ungroup() %>%
+  dplyr::mutate(precursor = case_when(
+    grepl("PF07862", pfam_id1) ~ "Nif11",
+    grepl("PF07862", pfam_id2) ~ "Nif11",
+    TRUE ~ "NHLP")) %>%
+  dplyr::mutate(nif11_queries = case_when(
+    precursor == "Nif11" ~ query,
+    TRUE ~ NA_character_)) %>%
+  dplyr::mutate(precursor_type = case_when(
+        query %in% nif11_queries ~ "Nif11",
+        TRUE ~ "NHLP")) %>%
+  dplyr::mutate(known_product = case_when(
+    grepl("WP_007357382.1", query) ~ "landornamide",
+    grepl("aerodenitrificans", genus_species) ~ "aeronamide",
+    grepl("polytheonamide", genus_species) ~ "polytheonamide",
+    grepl("WP_036784545", query) ~ "  ", # pha 
+    grepl("WP_007358513.1", query) ~ " ", # ksp
+    grepl("WP_012412980.1", query) ~ "    ", # npu
+    TRUE ~ "unknown"))
+
+nost <- dat[grep("WP_012412980.1", dat$query),]
+
+# Find nbs for characterized NPs
+char <- read_csv("data/characterized_nps/proteusin_key_nbs_pull/main_co_occur.csv")
+
+# Reshape data frome long to wide
+dat_trim <- dat
+mat <- reshape2::dcast(dat_trim, grouping_id ~ pfam_long, value.var = "pfam_count") 
+mat_bool <- mat # makes a copy
+mat_bool[mat > 1] <- 1 # dreates a boolean matrix
+
+# Barplot of most common co-occurring PFAMs
+bpldf <- data.frame(sort(colSums(mat_bool[,2:ncol(mat_bool)]), decreasing = T)[1:40]) %>%
+  rownames_to_column()
+colnames(bpldf) <- c("PFAM", "Count")
+
+bpldf <- bpldf %>% 
+  dplyr::filter(PFAM != "NA") %>%
+  dplyr::filter(PFAM != "NA_NA") %>%
+  dplyr::mutate(coloring = ifelse(PFAM %in% "PF00583_Acetyltransf_1", "forestgreen", "gray60")) %>%
+  dplyr::mutate("% BGCs containing PFAM" = round((Count/2347) * 100, 2))
+bpldf$PFAM <- factor(bpldf$PFAM, levels = rev(as.character(bpldf$PFAM)))
+
+pdf("output/Supplementary_fig_S1.pdf", width = 8.5, height = 12)
+bpl_s1 <- ggplot(data = bpldf, aes(x = PFAM, y = `% BGCs containing PFAM`)) + 
+  geom_bar(stat = "identity", fill = bpldf$coloring) + 
+  theme_pubr() +
+  scale_y_continuous(expand = expansion(mult = c(0, 0))) +
+  coord_flip()
+bpl_s1
+dev.off()
+
+bpl_for_tab <- bpldf %>%
+  dplyr::select(-coloring)
+
+# Trim to make barplots
+bpl_maturases <- bpldf %>%
+  dplyr::filter(PFAM %in% c("PF04055_Radical_SAM",
+                            "PF13575_DUF4135",
+                            "PF00583_Acetyltransf_1",
+                            "PF02624_YcaO",
+                            "PF00881_Nitroreductase",
+                            "PF13561_adh_short_C2",
+                            "PF13649_Methyltransf_25",
+                            "PF00106_adh_short",
+                            "PF01593_Amino_oxidase")) 
+
+bpl_maturases$PFAM <- gsub("_", " ", bpl_maturases$PFAM)
+bpl_maturases$PFAM[grep(" ", bpl_maturases$PFAM)]
+bpl_maturases$`Protein family` <- c("Radical SAM (PF04055)",
+               "DUF4135 (PF13575)",
+               "Acyltransferase (PF00583)",
+               "YcaO (PF02624)",
+               "Nitroreductase (PF00881)",
+               "Enoyl-ACP reductase (PF13561)",
+               "Methyltransferase (PF13649)",
+               "Short-chain dehydrogenase (PF00106)",
+               "Amine oxidoreductase (PF01593)")
+   
+bpl_maturases$Family<- factor(bpl_maturases$`Protein family`, 
+                              levels = rev(c("Radical SAM (PF04055)",
+                                        "DUF4135 (PF13575)",
+                                        "Acyltransferase (PF00583)",
+                                        "YcaO (PF02624)",
+                                        "Nitroreductase (PF00881)",
+                                        "Enoyl-ACP reductase (PF13561)",
+                                        "Methyltransferase (PF13649)",
+                                        "Short-chain dehydrogenase (PF00106)",
+                                        "Amine oxidoreductase (PF01593)")))
+
+bpl <- ggplot(data = bpl_maturases, aes(x = Family, y = `% BGCs containing PFAM`)) + 
+  geom_bar(stat = "identity", fill = bpl_maturases$coloring) + 
+  theme_pubr() +
+  theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0)))
+bpl
+
+# Matrix of PFAM clans
+keep_tax_info <- dat_trim %>%
+  dplyr::select(grouping_id, known_product, precursor_type, genvar, genus_species, 
+                nucleotide_acc, gtdb_tax, phylum, order, class, 
+                family, genus_1) %>%
+  distinct()
+
+mat_df <- mat %>%
+  right_join(keep_tax_info, ., by = "grouping_id") %>%
+  distinct(grouping_id, .keep_all = T) %>%
+  mutate(kingdom = "Bacteria")
+
+mat_num <- data.frame(mat_df[, grep("^PF", colnames(mat_df))]) 
+rownames(mat_num) <- mat_df$grouping_id
+
+# Remove columns with near zero variance using nearZeroVar fucntion
+nozdat <- nearZeroVar(mat_num, 
+                      freqCut = 98.9/1.1,
+                      saveMetrics = TRUE)
+which_rem <- rownames(nozdat)[nozdat[,"nzv"] == TRUE]
+
+findat <- mat_num %>%
+  dplyr::select(-all_of(which_rem))
+
+# Normalize across rows using sweep funciton
+findat[1:10, 1:10]
+mat_reg <- sweep(findat, 1, rowSums(findat), '/') 
+mat_fin <- mat_reg[!is.na(rowSums(mat_reg)),]
+
+# Run Principal Coordinates Analysis using vegan package
+d.jaccard <- vegdist(mat_fin, method = "jaccard") # jaccard distance metric
+pc.bray <- cmdscale(d.jaccard, eig = TRUE, k = 2)
+pc1 <- pc.bray$GOF[1]
+pc2 <- (pc.bray$GOF[2]-pc.bray$GOF[1])
+
+xlab <- paste0("PC 1 (",round(pc1*100,2),"% explained var.)")
+ylab <- paste0("PC 2 (",round(pc2*100,2),"% explained var.)")
+
+plotdf <- data.frame(grouping_id = rownames(pc.bray$points),
+                     PC1 = pc.bray$points[,1],
+                     PC2 = pc.bray$points[,2]) %>%
+  left_join(mat_df, by = "grouping_id")
+
+# Set custom color palette for plotting
+pal <- colorRampPalette(brewer.pal(8, "Set1"))(8)
+pal2 <- c(pal[c(2, 8, 5, 1, 4)], "seagreen2", "gray80",  "black")
+
+# PCoA of taxonomy assignments (phylum level)
+prod <- ggplot() + 
+  geom_point(data = plotdf,
+             alpha = ifelse(plotdf$known_product == "unknown", 0.4, 1),
+             size = ifelse(plotdf$known_product == "unknown", 1, 3),
+             aes(
+               x = PC1,
+               y = PC2,
+               color = known_product)) +
+  theme_pubr() +
+  theme(legend.text=element_text(size=14),
+    legend.title = element_text(size = 16),
+    legend.position = "none") +
+  scale_color_manual(values = pal2) +
+  geom_text_repel(data = subset(plotdf, !known_product %in% c("unknown", "npu", "ksp", "pha")),
+    size = 5,
+    aes(x = PC1,
+        y = PC2,
+        label = known_product)) +
+  guides(colour = guide_legend(override.aes = list(size=5),
+                               ncol = 2))
+
+# Calculate weighted averages of BGCs
+# Normalize by dividing by column sums
+pfams.norm <- sweep(mat_fin, 2, colSums(mat_fin), '/')
+
+# Use matrix multiplication to calculated weighted average 
+# of each PFAM Clan along each axis
+wa <- t(pfams.norm) %*% pc.bray$points[,1:2] # weighted average
+wa_df <- data.frame(CL = rownames(wa),
+                    WA_1 = wa[,1],
+                    WA_2 = wa[,2])
+
+# GNAT-family acyltransferases PF00583
+acetyl <- ggplot() +
+  geom_point( data = plotdf,
+              alpha = 0.8,
+              aes(
+                x = PC1,
+                y = PC2,
+                color = PF00583_Acetyltransf_1)) +
+  theme_pubr() +
+  scale_color_viridis(name = "GNAT-acyltransferases\n(PF00583)\nper BGC",
+                      direction = -1, 
+                      option = "magma",
+                      end = 0.9,
+                      na.value = "gray80") 
+acetyl
+
+# C39 Peptidases
+c39 <- ggplot() +
+  geom_point(data = plotdf,
+             alpha = 0.8,
+             aes(
+               x = PC1,
+               y = PC2,
+               color = PF03412_Peptidase_C39)) +
+  theme_pubr() +
+  scale_color_viridis(name = "Peptidase C39\n(PF03412)\nper BGC",
+                      direction = -1, 
+                      option = "magma",
+                      end = 0.9,
+                      na.value = "gray80")  #+
+c39
+
+abc <- ggplot() +
+  geom_point(data = plotdf,
+             alpha = 0.8,
+             aes(
+               x = PC1,
+               y = PC2,
+               color = PF00005_ABC_tran)) +
+  theme_pubr() +
+  scale_color_viridis(name = "ABC trans.\n(PF00005)\n per BGC",
+                      direction = -1, 
+                      option = "magma",
+                      end = 0.9,
+                      na.value = "gray80")  #+
+abc
+
+# Radical SAMs
+sam <- ggplot() +
+  geom_point(data = plotdf,
+             alpha = 0.6,
+             aes(
+               x = PC1,
+               y = PC2,
+               color = PF04055_Radical_SAM)) +
+  theme_pubr() +
+  scale_color_viridis(name ="Radical SAMs\n (PF04055)\n per BGC",
+                      direction = -1,
+                      option = "magma",
+                      end = 0.9,
+                      na.value = "gray80")
+sam
+
+# NHLP vs. Nif11 precursors
+colnames(plotdf)[grep("PF07862", colnames(plotdf))]
+nif11 <- ggplot() +
+  geom_point(data = plotdf,
+             alpha = 0.6,
+             aes(
+               x = PC1,
+               y = PC2,
+               color = precursor_type)) +
+  theme_pubr() +
+  #theme(axis.title.x = element_blank()) +
+  scale_color_manual(values = magma(8)[c(5, 7)],
+                     name = "RiPP\nprecursor\ntype")
+nif11
+
+# DUF4135
+duf4135 <- ggplot() +
+  geom_point(data = plotdf,
+             alpha = 0.6,
+             aes(
+               x = PC1,
+               y = PC2,
+               color = PF13575_DUF4135)) +
+  theme_classic() +
+  #theme(axis.title.x = element_blank()) +
+  scale_color_viridis(name ="DUF4135 \n  (PF13575) \n per BGC",
+                      direction = -1,
+                      option = "magma",
+                      end = 0.9,
+                      na.value = "gray80")  #+
+duf4135
+
+# Set additional colors in custom palette
+pal_distinct <- palette(colorRampPalette(brewer.pal(8,"Set2"))(8))
+colorblind <- c(pal[1], "#CC6677",  "#999933",  "steelblue4", "#AA4499",  
+                 pal_distinct[5:7], 
+                "#882255", "#6699CC",  pal_distinct[2:3], "gray40",
+                "navyblue", "mistyrose1", "#88CCEE", pal_distinct[4], 
+                "orange", "goldenrod", "purple4", "lightseagreen")
+plotdf$phylum[is.na(plotdf$phylum)] <- "Other"
+
+cr_df <- data.frame(sort(table(plotdf$phylum), decreasing = T), stringsAsFactors = F) 
+colnames(cr_df) <- c("Phylum", "Freq")
+
+cr_df$Phylum <- as.character(cr_df$Phylum)
+cr_ord <- cr_df[order(cr_df$Phylum),] # order alphabetically
+cr_ord$lab <- round((cr_ord$Freq/sum(cr_ord$Freq) * 100), 1)
+cr_ord$lab[cr_ord$lab < 3.0] <- ""
+cr_ord$lab <- ifelse(cr_ord$lab == "", "", paste0(cr_ord$lab, "%"))
+
+donut <- ggdonutchart(data = cr_ord,
+                      x = "Freq",
+                      label = "lab",
+                      fill = "Phylum",
+                      lab.font = c(6, "plain", "black"),
+                      color = "white",
+                      palette = colorblind)
+donut
+
+phygraph <- ggplot(data = plotdf) +
+  geom_point(data = plotdf,
+             alpha = 0.8,
+             size = 2,
+             aes(
+               x = PC1,
+               y = PC2,
+               color = phylum)) +
+  scale_color_manual(values = colorblind,
+                     name = "Phylum") +
+  theme_pubr()  +
+  guides(colour = guide_legend(override.aes = list(size= 3),
+                               ncol = 1))
+phygraph
+
+pdf("output/legend.pdf")
+donut <- donut +
+  ggtitle('Z') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_blank(),
+        plot.title = element_blank(),
+        legend.position = "left")
+donut
+dev.off()
+
+phygraph <- phygraph +
+  ggtitle('A') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18),
+        legend.text=element_text(size = 14),
+        legend.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.position = "none")
+
+donut <- donut +
+  ggtitle('Z') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_blank(),
+        plot.title = element_blank(),
+        legend.position = "none")
+prod <- prod +
+  ggtitle('B') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18),
+        plot.title = element_text(size = 24))
+bpl <- bpl +
+  ggtitle('C') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18),
+        plot.title = element_text(size = 24),
+        axis.title.y = element_blank()) +
+  coord_flip()
+bpl
+
+c39 <- c39 + 
+  ggtitle('F') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        legend.position = "right")
+abc <- abc +
+  ggtitle('E') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        legend.position = "right")
+nif11 <- nif11 +
+  ggtitle('D') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size =  24),
+        legend.text=element_text(size=16),
+        legend.title = element_text(size = 14),
+        legend.position = "right")
+acetyl <- acetyl +
+  ggtitle('G') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        legend.position = "right")
+
+layout <- '
+AAAAAABBBCCCCCCCC
+DDDDDEEEHHHHHHHHH
+DDDDDEEEHHHHHHHHH
+FFFFFGGGHHHHHHHHH
+FFFFFGGGHHHHHHHHH'
+
+pdf("output/Figure1.pdf", width = 21, height = 14)
+wrap_plots(
+  A = prod,
+  B = phygraph,
+  C = donut,
+  D = nif11,
+  E = abc,
+  F = c39,
+  G = acetyl,
+  H = bpl,
+  heights = c(8,1.5,1.5,1.5,1.5),
+  design = layout)
+dev.off()
+
+layout1 <- '
+AAAABBBB
+DDDDCCC#'
+
+# New top-half
+pdf("output/Figure1_part1.pdf", width = 15, height = 12)
+wrap_plots(
+  A = phygraph,
+  B = donut,
+  C = bpl,
+  D = prod,
+  heights = c(3,3),
+  widths = c(2,1),
+  design = layout1)
+dev.off()
+
+layout2 <-'
+EEEFFF
+GGGHHH'
+
+pdf("output/Figure1_part2.pdf", width = 15, height = 10)
+wrap_plots(  
+  E = nif11,
+  F = abc,
+  G = c39,
+  H = acetyl,
+  widths = c(1,1,1,1),
+  design = layout2)
+dev.off()
+
+c39 <- c39 + 
+  ggtitle('F') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=12),
+        legend.title = element_text(size = 14),
+        legend.position = "top")
+abc <- abc +
+  ggtitle('E') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=12),
+        legend.title = element_text(size = 14),
+        legend.position = "top")
+nif11 <- nif11 +
+  ggtitle('D') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size =  24),
+        legend.text=element_text(size=14),
+        legend.title = element_text(size = 14),
+        legend.position = "top")
+acetyl <- acetyl +
+  ggtitle('G') +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16),
+        plot.title = element_text(size = 24),
+        legend.text=element_text(size=12),
+        legend.title = element_text(size = 14),
+        legend.position = "top")
+
+layout3 <-'
+EEEFFFGGGHHH'
+
+pdf("output/Figure1_part2.pdf", width = 15, height = 5)
+wrap_plots(  
+  E = nif11,
+  F = abc,
+  G = c39,
+  H = acetyl,
+  widths = c(1,1,1,1),
+  design = layout3)
+dev.off()
+
+
